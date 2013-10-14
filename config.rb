f6e23b22dfc8e1acb27c7cf8d0fedfc25396949b
joaomamede <joaovpmamede@gmail.com>
2013-10-14 14:57:10 +0100

    compass stuff

--- config.rb
+++ config.rb
@@ -9,6 +9,7 @@ javascripts_dir = "javascripts/"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
@@ -22,4 +23,4 @@ javascripts_dir = "javascripts/"
 # preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
-preferred_syntax = :scss
+#preferred_syntax = :scss
