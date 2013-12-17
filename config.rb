f94188b741fae0c236dea9412bd16692d7adf9d3
joaomamede <joaovpmamede@gmail.com>
2013-12-17 00:38:26 +0000

    configs for sass

--- config.rb
+++ config.rb
@@ -2,7 +2,7 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css/"
+css_dir = "_site/css/"
 sass_dir = "css/src/"
 images_dir = "images/"
 javascripts_dir = "javascripts/"
