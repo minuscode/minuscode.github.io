661fa52505c197105ff42dac07a357148f7c36d0
Miguel Marinhas <miguel.marinhas@gmail.com>
2014-01-31 01:05:19 +0000

    - added redcloth gem as dependency for development - removed old stuff from the previous v1.0 website - changed config.rb a bit

--- config.rb
+++ config.rb
@@ -2,14 +2,16 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "_site/css/"
-sass_dir = "css/src/"
-images_dir = "images/"
-javascripts_dir = "javascripts/"
+sass_dir = "../css/src/"
+css_dir = "../css"
+images_dir = "../img"
+javascripts_dir = "../js"
+relative_assets = true
+
+#output_style = :compressed
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
@@ -23,4 +25,3 @@ output_style = :compressed
 # preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
-#preferred_syntax = :scss
