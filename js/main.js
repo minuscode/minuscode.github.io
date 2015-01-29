497bfe59501fca5a09520b047798d97c5ead0564
Luis Rita <luisrita@luiss-mbp.lan>
2015-01-29 12:54:40 +0000

    created function to hide sercret projects

--- js/main.js
+++ js/main.js
@@ -32,19 +32,24 @@ MINUS = (function($) {
 
 })(jQuery);
 
+
 // Work page
 //-----------------------------
 MINUS.work = (function($) {
   var categoryList = $('.categories'),
       workItem = $('.work-item');
 
-  categoryList.delegate('a', 'click', function(e) {
-    e.preventDefault();
-    var catName = $(this).data('category');
+  function init() {
+    categoryList.delegate('a', 'click', function(e) {
+      e.preventDefault();
+      var catName = $(this).data('category');
 
-    changeLinkState($(this));
-    hideWorkItems(catName);
-  });
+      changeLinkState($(this));
+      hideWorkItems(catName);
+    });
+
+    window.onhashchange = showAllProjects();
+  }
 
   function changeLinkState(clicked) {
     categoryList.find('li a').removeClass('active');
@@ -73,5 +78,23 @@ MINUS.work = (function($) {
       });
     }
   }
+
+  function showAllProjects() {
+    if(location.search === '?showall') {
+      $('.hidden').css('display', 'inline');
+    }
+  }
+
+  return {
+    init: init
+  };
+
+})(jQuery);
+
+// Init all the work things
+//-----------------------------
+(function($) {
+
+  MINUS.work.init();
 
 })(jQuery);
