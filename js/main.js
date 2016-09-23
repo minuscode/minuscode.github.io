d10783435fad31d77af95dc4d9c20850cf2482a1
csergiuu <sergiumultimedia@gmail.com>
2016-09-23 11:51:37 +0100

    new work page

--- js/main.js
+++ js/main.js
@@ -85,6 +85,34 @@ MINUS.work = (function($) {
     }
   }
 
+  setInterval(function(){
+    $('.line-chart span').addClass('is-active');
+  }, 500);
+
+
+  var animation_elements = $('.work-wrap');
+  
+  function check_if_in_view() {
+    var window_height = $(window).height();
+    var window_top_position = $(window).scrollTop();
+    var window_bottom_position = (window_top_position + window_height);
+
+    $.each(animation_elements, function() {
+      var element = $(this);
+      var element_height = element.outerHeight();
+      var element_top_position = element.offset().top;
+      var element_bottom_position = (element.offset().top + element_height);
+
+      
+      if ((element_bottom_position >= window_top_position) && (element_top_position <= window_bottom_position)) {
+        element.addClass('in-view');
+      }
+    });
+  }
+  
+  $(window).on('scroll resize', check_if_in_view);
+  $(window).trigger('scroll');
+
   return {
     init: init
   };
@@ -115,6 +143,6 @@ MINUS.about_us = (function($) {
 //-----------------------------
 (function($) {
 
-  MINUS.work.init();
+  MINUS.work.init(); 
 
 })(jQuery);
